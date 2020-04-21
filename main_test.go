5ee014310d07fbaa5ba85aa9e731507de324b6a5
arsmn <arsmn1996@gmail.com>
2020-04-21 16:29:47 +0430

    add tests

--- main_test.go
+++ main_test.go
@@ -4,3 +4,102 @@
 // 🙏 Thanks & Credits to @arsmn
 
 package embed
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/gobuffalo/packr/v2"
+	"github.com/gofiber/fiber"
+	"github.com/markbates/pkger"
+)
+
+func Test_Embed(t *testing.T) {
+	app := *fiber.New()
+
+	// pkger
+	app.Use(New(Config{
+		Prefix: "/pkger/",
+		Root:   pkger.Dir("/testdata"),
+	}))
+
+	// packr
+	app.Use(New(Config{
+		Prefix: "/packr/",
+		Root:   packr.New("box", "./testdata"),
+	}))
+
+	// go.rice
+	app.Use(New(Config{
+		Prefix: "/rice/",
+		Root:   rice.MustFindBox("./testdata").HTTPBox(),
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) {
+		c.SendString("Hello, World!")
+	})
+
+	embedders := []string{"pkger", "packr", "rice"}
+
+	tests := []struct {
+		name        string
+		fileName    string
+		statusCode  int
+		contentType string
+	}{
+		{
+			name:        "Should be returns status 200 with suitable content-type",
+			fileName:    "index.html",
+			statusCode:  200,
+			contentType: "text/html; charset=utf-8",
+		},
+		{
+			name:        "Should be returns status 200 with suitable content-type",
+			fileName:    "test.json",
+			statusCode:  200,
+			contentType: "application/json",
+		},
+		{
+			name:        "Should be returns status 200 with suitable content-type",
+			fileName:    "main.css",
+			statusCode:  200,
+			contentType: "text/css; charset=utf-8",
+		},
+		{
+			name:        "Should be returns status 404",
+			fileName:    "nofile.js",
+			statusCode:  404,
+			contentType: "text/plain; charset=utf-8",
+		},
+		{
+			name:        "Should be returns status 404",
+			fileName:    "nofile",
+			statusCode:  404,
+			contentType: "text/plain; charset=utf-8",
+		},
+	}
+
+	for _, embedder := range embedders {
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				url := fmt.Sprintf("/%s/%s", embedder, tt.fileName)
+				req, _ := http.NewRequest("GET", url, nil)
+				resp, err := app.Test(req)
+				if err != nil {
+					t.Fatalf(`%s: %s`, t.Name(), err)
+				}
+
+				if resp.StatusCode != tt.statusCode {
+					t.Fatalf(`%s: StatusCode: got %v - expected %v`, t.Name(), resp.StatusCode, tt.statusCode)
+				}
+
+				ct := resp.Header.Get("Content-Type")
+				if ct != tt.contentType {
+					t.Fatalf(`%s: Content-Type: got %s - expected %s`, t.Name(), ct, tt.contentType)
+				}
+			})
+		}
+	}
+}
